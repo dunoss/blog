fdb6836da3060d347f431675251ffdc6018becc7
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-06-16 15:56:38 -0400

    added edit and delete

--- app/controllers/flogs_controller.rb
+++ app/controllers/flogs_controller.rb
@@ -17,6 +17,26 @@ class FlogsController < ApplicationController
     @flog = Flog.find(params[:id])
   end
 
+  def edit
+    @flog = Flog.find(params[:id])
+  end
+
+  def update
+    @flog = Flog.find(params[:id])
+
+    if @flog.update(params[:flog].permit(:who, :date, :location, :description))
+      redirect_to @flog
+    else render 'edit'
+    end
+  end
+
+  def destroy
+    @flog = Flog.find(params[:id])
+    @flog.destroy
+
+    redirect_to flogs_path
+  end
+
   private
     def flog_params
       params.require(:flog).permit(:date, :location, :who, :description)
